e712b6d2920b244d4bd4cffb21e950f218542aaf
cebms <cemendes@gmail.com>
2023-05-27 16:21:58 -0300

    original image crop

--- main.py
+++ main.py
@@ -15,8 +15,11 @@ if __name__ == "__main__":
     detector = PlateDetector(args.weights)
     plates = detector.detect_plates(image)    
 
+    crops = []
     for plate in plates:
+        crops.append(image[plate[0][1]:plate[1][1], plate[0][0]:plate[1][0]])
         cv2.rectangle(image, plate[0], plate[1], (0, 255, 0), 2)
 
-    cv2.imshow("frame", image)
+
+    cv2.imshow("frame", crops[0])
     cv2.waitKey(0)
